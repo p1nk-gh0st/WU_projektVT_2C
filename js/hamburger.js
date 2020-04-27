4218ede314a54d0292a1a17416556404cecf0fc1
p1nk-gh0st <55193993+p1nk-gh0st@users.noreply.github.com>
2020-04-27 18:25:09 +0200

    ;)))

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('.hamburger');
+const mobileNavbar = document.querySelector('.mobileNav');
+const body = document.querySelector('body');
 
 button.addEventListener('click', () => {
-    button.classList.toggle('is-active')
+    button.classList.toggle('is-active');
+    mobileNavbar.classList.toggle('show');
+    body.classList.toggle('no-scroll');
 })

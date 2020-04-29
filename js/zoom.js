7e9e66de742d2cfb3e8751868960d5c71d46c03c
p1nk-gh0st <55193993+p1nk-gh0st@users.noreply.github.com>
2020-04-29 18:01:52 +0200

    see changelog ;)

---
+++ js/zoom.js
@@ -0,0 +1,13 @@
+mediumZoom('.galleryDesktop', {
+    background: '#000000',
+    margin: 40,
+    scrollOffset: 9000,
+
+})
+
+mediumZoom('.galleryMobile', {
+    background: '#000000',
+    margin: 40,
+    scrollOffset: 9000,
+
+})

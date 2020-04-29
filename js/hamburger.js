ce4357465663372fa2f312bee363fe9c75bc30ba
p1nk-gh0st <55193993+p1nk-gh0st@users.noreply.github.com>
2020-04-29 13:12:28 +0200

    small changes, bug fixes, check changelog

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,13 @@
 const button = document.querySelector('.hamburger');
 const mobileNavbar = document.querySelector('.mobileNav');
-const body = document.querySelector('body');
+const all = document.querySelector('*');
+const mobileNavbaralt = document.querySelector('.mobile-Navbar');
 
 button.addEventListener('click', () => {
     button.classList.toggle('is-active');
     mobileNavbar.classList.toggle('show');
-    body.classList.toggle('no-scroll');
+    all.classList.toggle('no-scroll');
+    mobileNavbaralt.classList.toggle('no-shadow')
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 })
